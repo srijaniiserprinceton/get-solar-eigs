0ece24aadbd697e531e6b594a7eb75cec5ea620a
srijaniiserprinceton <sbdas@princeton.edu>
2021-04-13 18:11:23 -0400

    Moving ws/w.dat to snrnmais_files/data_files for ease of usage

--- efs_Antia/make_config.py
+++ efs_Antia/make_config.py
@@ -44,6 +44,7 @@ def create_makefile():
     f.write("\t# generating the w_s data from data/w_s/w_samarth.dat\n")
     f.write(f"\t@python {datadir}/write_w.py\n")
     f.write(f"\t@rm {datadir}/data/w_s/w_samarth.dat\n")
+    f.write(f"\t@mv {datadir}/data/w_s {snrdir}/data_files/.\n")
     f.write(f"\t@rm -rf {datadir}/dlfiles\n")
     f.write(f"\t@rm {snrdir}/data_files/eigU.dat\n")
     f.write(f"\t@rm {snrdir}/data_files/eigV.dat\n")
