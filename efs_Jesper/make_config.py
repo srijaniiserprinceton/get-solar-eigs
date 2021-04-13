5d644849c105601d3f4ea339ddce757087ef0a28
srijaniiserprinceton <sbdas@princeton.edu>
2021-04-13 12:16:37 -0400

    Allows conditional use of the tar ball in the cloud rather than IDL

--- efs_Jesper/make_config.py
+++ efs_Jesper/make_config.py
@@ -7,7 +7,7 @@ cwd = os.getcwd()
 sys.path.append(cwd)
 NL = "\n"
 
-def create_makefile():
+def create_makefile_idl():
     f = open(f"{cwd}/makefile", "w")
     f.write("# .PHONY defines parts of the makefile that ")
     f.write("are not dependant on any specific file\n")
@@ -60,6 +60,52 @@ def create_makefile():
     return None
 
 
+def create_makefile_noidl():
+    f = open(f"{cwd}/makefile", "w")
+    f.write("# .PHONY defines parts of the makefile that ")
+    f.write("are not dependant on any specific file\n")
+    f.write(".PHONY = help setup test run clean environment\n\n")
+    f.write("# Defines the default target that `make` will to try to make, ")
+    f.write("or in the case of a phony target, execute the specified commands\n")
+    f.write("# This target is executed whenever we just type `make`\n")
+    f.write(".DEFAULT_GOAL = setup\n")
+    f.write("# The @ makes sure that the command itself ")
+    f.write("isnt echoed in the terminal\n")
+    f.write("setup:\n")
+    f.write(f"\t@tar -xf {dldir}/efs_Jesper.tar.gz\n")
+    f.write(f"\t@mv {dldir}/w_samarth.dat {datadir}/data/w_s/.\n")
+    # f.write("# making necessary directory structure\n")
+    # f.write("\t# cloning the GitHub repo to get eig_files and data_files to store in snrnmais\n")
+    # f.write(f"\t@git clone https://github.com/srijaniiserprinceton/get-solar-eigs.git {outdir}\n")
+    # f.write(f"\t@mv {dldir}/* {outdir}/.\n")
+    # f.write(f"\t@mv {outdir}/*.dat {datadir}/data/w_s/.\n")
+    # if not (cwd == datadir):
+    #     f.write(f"\t@cp {cwd}/data/w_s/write_w.py {datadir}/data/w_s/.\n")
+    #     f.write(f"\t@cp {cwd}/w.dat {datadir}/data/w.dat\n")
+    # f.write(f"\t@make -C {outdir}/\n")
+    # f.write(f"\t@mv {outdir}/eig_files {datadir}/snrnmais_files/.\n")
+    # f.write(f"\t@mv {outdir}/data_files {datadir}/snrnmais_files/.\n")
+    # f.write(f"\t@rm -rf {outdir}\n")
+    # f.write(f"\t@make -C \n")
+    f.write("\t# generating the w_s data from data/w_s/w_samarth.dat\n")
+    f.write(f"\t@python {datadir}/write_w.py\n")
+    f.write(f"\t@rm {datadir}/data/w_s/w_samarth.dat\n")
+    f.write(f"\t@rm -rf {datadir}/dlfiles\n")
+    f.write("\t@echo '---------------------------------------------------------------------'\n")
+    f.write("\t@echo '       Eigenfrequency and Eigenfunciton SUCCESSFULLY GENERATED       '\n")
+    f.write("\t@echo '---------------------------------------------------------------------'\n")
+    f.write("help:\n")
+    f.write("\t@echo '---------------------------------HELP--------------------------------'\n")
+    f.write("\t@echo 'TO EXTRACT EIGENFREQUENCIES AND EIGENFUNCTIONS: python make_config.py'\n")
+    f.write("\t@echo '-------------------------------------------------------------------'\n\n")
+    f.write("clean:\n")
+    f.write(f"\t@rm -rf {datadir}/snrnmais_files\n")
+    f.write(f"\t@rm -rf {dldir}\n")
+    f.write(f"\t@rm -rf {datadir}/data/w_s")
+    # f.write(f"\t@rm -rf *.egg-info\n")
+    return None
+
+
 def create_dir(fulldirpath):
     if not os.path.isdir(fulldirpath):
         print(f"Creating directory {fulldirpath}")
@@ -85,8 +131,13 @@ def reporthook(count, block_size, total_size):
 def urlsave(url, filename):
     urllib.request.urlretrieve(url, filename, reporthook)
 
-def download_files():
-    with open(f'{cwd}/.dl_files', 'r') as f:
+def download_files(has_idl):
+    if(has_idl):
+        dlfiles_fname = f'{cwd}/.dl_files_idl'
+    else:
+        dlfiles_fname = f'{cwd}/.dl_files_noidl'
+
+    with open(dlfiles_fname, 'r') as f:
         urlnames = f.read().splitlines()
 
     for url in urlnames:
@@ -102,18 +153,31 @@ def download_files():
 if __name__ == "__main__":
     datadir = input(f"Enter location to store the eigenfunctions and output files:" +
                     f"Default: {cwd} -- ") or f"{cwd}"
+
+    while(True):
+        idl_y_or_n = input("Do you have IDL on your system? (y/n)")
+        if(idl_y_or_n == 'y' or idl_y_or_n == 'Y'): 
+            has_idl = True
+            break
+        elif(idl_y_or_n == 'n' or idl_y_or_n == 'N'): 
+            has_idl = False
+            break
+        else:
+            print("Wrong option. Choose y or n.")
+            continue
+
     dldir = f"{datadir}/dlfiles"
     snrdir = f"{datadir}/snrnmais_files"
     create_dir(datadir)
     create_dir(snrdir)
     create_dir(dldir)
     create_dir(f"{datadir}/data/w_s")
-    # temporary directory to be deleted later
-    # this is already created because of git cloning in the make file
+
     with open(f"{cwd}/.config", "w") as f:
         f.write(f"{cwd}{NL}")
         f.write(f"{datadir}")
 
-    download_files()
-    create_makefile()
+    download_files(has_idl)
+    if(has_idl): create_makefile_idl()
+    else: create_makefile_noidl()
     os.system("make")
